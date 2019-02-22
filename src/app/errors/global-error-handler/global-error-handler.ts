f88707aac3232d14d90520fddd5cc2457b6c7b1a
Talison <talisondaniel@hotmail.com>
2019-02-22 08:43:13 -0300

    Global Error Handler

---
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -0,0 +1,38 @@
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import * as StackTrace from 'stacktrace-js';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { UserService } from 'src/app/core/user/user.service';
+
+@Injectable()
+export class GlobalErrorHandler implements ErrorHandler {
+
+    constructor(private injector: Injector) {}
+    
+    handleError(error: any): void {
+        console.log('passei pelo handler');
+
+        const location = this.injector.get(LocationStrategy);
+        const userService = this.injector.get(UserService);
+
+        const url = location instanceof PathLocationStrategy 
+            ? location.path() 
+            : '';
+
+        const message = error.message 
+            ? error.message 
+            : error.toString();
+
+        StackTrace
+            .fromError(error)
+            .then(stackFrames => {
+                const stackAsString = stackFrames
+                    .map(sf => sf.toString())
+                    .join('\n');
+                
+                console.log(message);
+                console.log(stackAsString);
+
+                console.log({ message, url, userName:userService.getUserName(), stack: stackAsString });
+            })
+    }
+}
